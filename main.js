020c47140f51102b3596037f5ea086c7a9cfda7f
João Daniel Purificação <jota.daniel.38@gmail.com>
2022-09-12 21:26:54 -0300

    Operador Rest

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
-// Nullish Coalescing Operator (??)
-const idade = 0 ?? 'Não informado';
-document.body.innerText = 'Sua idade é: ' + idade;
+// Objetos
+const user = {
+  name: 'Diego',
+  idade: 27,
+  address:{
+    street: 'Rua Teste',
+    number: 176,
+  },
+};
+
+//document.body.innerText = ('name' in user) //Validar se existe uma chave no objeto
+
+// document.body.innerText = Object.keys(user) //Retorna as chaves do objeto
+
+// document.body.innerText = Object.values(user) //Retorna os valores das chaves do objeto
+
+// document.body.innerText = Object.entries(user) //Retorna vetores contendo chaves e valores associados 
+
+// Desestruturação = Remover parte do objeto para uma variável independente
+
+//Ambas as linhas retornam o mesmo resultado
+// const address =  user.address
+const {address, idade: age, nickname = 'Fernandes'} = user
+
+document.body.innerText = JSON.stringify({address, age, nickname})
+
+//Rest ou Resto
+
+const { name, idade, ...rest } = user
+const array = [1,2,3,4,5,6,7,8,9,10]
+
+// Desestruturação
+// const first = array[0];
+const [first, second, ...restante] = array
+
+const [primeiro, , terceiro, ...todoResto] = array
+
