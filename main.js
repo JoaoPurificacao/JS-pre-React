b01de45328788e894cfe6a6ad0e4e9f62a997457
João Daniel Purificação <jota.daniel.38@gmail.com>
2022-09-12 21:32:23 -0300

    Short Sintax

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // Objetos
-const user = {
-  name: 'Diego',
-  idade: 27,
-  address:{
-    street: 'Rua Teste',
-    number: 176,
-  },
-};
+// const user = {
+//   name: 'Diego',
+//   idade: 27,
+//   address:{
+//     street: 'Rua Teste',
+//     number: 176,
+//   },
+// };
 
 //document.body.innerText = ('name' in user) //Validar se existe uma chave no objeto
 
@@ -26,12 +26,26 @@ document.body.innerText = JSON.stringify({address, age, nickname})
 
 //Rest ou Resto
 
-const { name, idade, ...rest } = user
-const array = [1,2,3,4,5,6,7,8,9,10]
+// const { name, idade, ...rest } = user
+// const array = [1,2,3,4,5,6,7,8,9,10]
 
-// Desestruturação
-// const first = array[0];
-const [first, second, ...restante] = array
+// // Desestruturação
+// // const first = array[0];
+// const [first, second, ...restante] = array
 
-const [primeiro, , terceiro, ...todoResto] = array
+// const [primeiro, , terceiro, ...todoResto] = array
 
+const nome = 'Diego';
+const idade = 27;
+
+const user = {
+  nome,
+  idade,
+};
+
+
+// A sintaxe abaixo produz o mesmo resultado que a sintaxe acima  
+// const user = {
+//   nome: nome,
+//   idade: idade,
+// };
